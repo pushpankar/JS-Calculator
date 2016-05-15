c9bfc4004026f41b9c4abaea88b63368c3e25974
Pushpankar <pushpankark@gmail.com>
2016-05-16 04:44:30 +0530

    Initial commit-With basic html and css

---
+++ Calculator.js
@@ -0,0 +1,9 @@
+var getRow = function(item1, item2, item3, item4){
+    if(arguments.length === 4){
+	var row  ;
+    }
+}
+var init = function(){
+    
+}
+document.addEventListener("DOMContentLoaded", init);
