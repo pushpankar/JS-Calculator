1d78fe74d8396dbfbe1099b4ef011e8ad17218d1
Pushpankar <pushpankark@gmail.com>
2016-05-21 21:29:37 +0530

    Create simple Calculator view

--- Calculator.js
+++ Calculator.js
@@ -1,9 +1,23 @@
+var id = 0;
+var text = ["AC", "CE", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", ".", "0", "Ans", "="];
 var getRow = function(item1, item2, item3, item4){
-    if(arguments.length === 4){
-	var row  ;
+    var each = "";
+    for(var i = 0; i < 4; i++){
+	each += "<h3 class='row_item' id='i"+id+"'>"+text[id]+"</h3>";
+	id++;
     }
+    return "<div class='row'>"+each+"</div>";
 }
+
 var init = function(){
+    var row =  "";
+    for(var i = 0; i < 4; i++){
+	row += getRow();
+    }
+    var element = document.getElementsByClassName("buttons");
+    //  element.innerHTML = "hello";
+    $(".buttons").html(row);
     
 }
+//window.onload = init;
 document.addEventListener("DOMContentLoaded", init);
