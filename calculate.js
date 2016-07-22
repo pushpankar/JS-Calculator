31f8fbade1befab6234f349d8b97c3a598e738eb
Pushpankar <pushpankark@gmail.com>
2016-07-22 19:50:07 +0530

    Fully Working Calculator

--- calculate.js
+++ calculate.js
@@ -3,20 +3,56 @@
  */
 
 var takeInput = function(obj){
-  console.log(obj.id);
-  var text = document.getElementById(obj.id).innerHTML;
-  var currentText = document.getElementById("result").innerHTML ;
-  document.getElementById("result").innerHTML = currentText + text;
-  work[obj.id]();
+    if(obj.id === "i0"){
+	document.getElementById("result").innerHTML="";
+    }else if(obj.id === "i1"){
+	toRPN(document.getElementById("result").innerHTML);
+	calc();
+	document.getElementById("result").innerHTML = "" + stack.pop();
+    }else{
+	var text = document.getElementById(obj.id).innerHTML;
+	if(obj.id === "i3" || obj.id === "i7" || obj.id === "i11" || obj.id === "i15"){
+	    updateExpr(" " + text + " ");
+	}else{
+	    updateExpr(text);
+	}
+    }
 }
 
-var work = {
-  i0 : function(){document.getElementById("result").innerHTML="";},
-  i1 : function(){
-    var text = document.getElementById("result").innerHTML;
-    var newText = text.substr(0, text.length - 3);
-    console.log(newText);
-    document.getElementById("result").innerHTML = newText;
-  }
+
+function updateExpr(text){
+    var elem = document.getElementById("result");
+    elem.innerHTML = elem.innerHTML + text;
+}
+function calc(){
+    while(queue.peek()){
+	if(Number.isInteger(parseInt(queue.peek()))){
+	    stack.add(parseInt(queue.pop()));
+	}else{
+	    var result;
+	    var operator = queue.pop();
+	    var num2 = stack.pop();
+	    var num1 = stack.pop();
+	    if(operator === "+"){
+		result = num1 + num2;
+		stack.add(result);
+	    }else if(operator === "-"){
+		result = num1 - num2;
+		stack.add(result);
+	    }else if(operator === "*"){
+		result = num1 * num2;
+		stack.add(result);
+	    }else if(operator === "/"){
+		result = num1 / num2;
+		stack.add(result);
+	    }
+	}
+    }
+}
+function isOperator(token){
+    if(token === "+" || token === "-" || token === "*" || token === "/"){
+	return true;
+    }
+    return false;
 }
 
